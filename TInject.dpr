09eaa96e39bfa7a8718abfa745fc050080d6d488
Mike <mikelustosa@gmail.com>
2019-10-23 01:22:12 -0300

    Adicionado aviso importante
    Adicionado aviso importante: Visualize_importante.jpeg
    
    Configurações do CEF na view source.

--- TInject.dpr
+++ TInject.dpr
@@ -17,37 +17,10 @@ uses
 
 begin
   GlobalCEFApp := TCefApplication.Create;
-
-
-  // In case you want to use custom directories for the CEF3 binaries, cache and user data.
-  // If you don't set a cache directory the browser will use in-memory cache.
-
-  //GlobalCEFApp.FrameworkDirPath     := 'BIN';
-  //GlobalCEFApp.ResourcesDirPath     := 'BIN';
-  //GlobalCEFApp.LocalesDirPath       := 'BIN\locales';
-
-  // Enable hardware acceleration
   GlobalCEFApp.EnableGPU            := True;
   GlobalCEFApp.cache                := 'DATA\cache';
+  GlobalCEFApp.DisableFeatures      := 'NetworkService,OutOfBlinkCors';
 
-
-  //Ativa log de depuração
-  //GlobalCEFApp.LogFile              := 'DATA\log';
-  //GlobalCEFApp.LogSeverity          := LOGSEVERITY_VERBOSE; // use LOGSEVERITY_VERBOSE if you want more details
-
-
-  //GlobalCEFApp.UserDataPath         := 'BIN\User Data';
-
-
-  //Ativa escuta http na porta 8077 ex: localhost:8077
-  //GlobalCEFApp.RemoteDebuggingPort := 8077;
-
-  // Disabling some features to improve stability
-  GlobalCEFApp.DisableFeatures  := 'NetworkService,OutOfBlinkCors';
-
-  // You *MUST* call GlobalCEFApp.StartMainProcess in a if..then clause
-  // with the Application initialization inside the begin..end.
-  // Read this https://www.briskbard.com/index.php?lang=en&pageid=cef
   if GlobalCEFApp.StartMainProcess then
     begin
       Application.Initialize;
@@ -55,7 +28,7 @@ begin
       Application.MainFormOnTaskbar := True;
       {$ENDIF}
       Application.CreateForm(Tfrm_principal, frm_principal);
-  Application.Run;
+      Application.Run;
     end;
 
   GlobalCEFApp.Free;
